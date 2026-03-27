5cbe1ad6c00025f68c49a53b5f57b4f02823c6d6
John LeClair <johnlleclair@gmail.com>
2026-03-27 10:45:14 -0400

    Update emfitarget.py

--- xyz-scanner/src/emfitarget.py
+++ xyz-scanner/src/emfitarget.py
@@ -1,3 +1,28 @@
+# ==========================================================================
+# Copyright (c) 2014-2018, NewAE Technology Inc
+# All rights reserved.
+#
+# Find this and more at newae.com - this file is part of the chipwhisperer
+# project, http://www.chipwhisperer.com
+#
+#    This file is part of chipwhisperer.
+#
+#    chipwhisperer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    chipwhisperer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
+# ChipWhisperer is a trademark of NewAE Technology Inc., registered in the
+# United States of America, the European Union, and other jurisdictions.
+# ==========================================================================
+
 import random
 
 import numpy as np
