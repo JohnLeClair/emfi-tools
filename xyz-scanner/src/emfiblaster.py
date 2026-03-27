57bdc5f738fbadc03fdbd6a563d879dd3d981509
John LeClair <johnlleclair@gmail.com>
2026-03-27 10:43:26 -0400

    Update emfiblaster.py

--- xyz-scanner/src/emfiblaster.py
+++ xyz-scanner/src/emfiblaster.py
@@ -1,3 +1,28 @@
+# Copyright (c) 2025    Condor Embedded Technology, LLC
+# All rights reserved.
+#
+# Author: John LeClair
+# Email:  jleclair@condorembeddedtech.com
+#
+# filename: xy_stage.py
+# This file is part of the EMFI-Target project which is based at least in part
+# by NewAE Ballistic Gel project hardware and software. 
+#
+#    This project is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This project is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this project.  If not, see <http://www.gnu.org/licenses/>.
+#=============================================================================
+
+
 import serial
 import time
 
